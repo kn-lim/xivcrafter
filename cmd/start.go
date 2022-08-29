a5353b3819e61951db71babc70aeefabcf1f4916
Kevin Lim <knlim95@gmail.com>
2022-08-28 23:02:57 -0700

    Check flags are valid

--- cmd/start.go
+++ cmd/start.go
@@ -37,8 +37,8 @@ var startCmd = &cobra.Command{
 		xiv := new(crafter.XIVCrafter)
 		xiv.Init(food, foodDuration, potion, amount, macro1, macro1Duration, macro2, macro2Duration, confirm, cancel, startPause, stop)
 
-		// check if all keys are valid
-		if !crafter.CheckKeys(*xiv) {
+		// check if all keys and flags are valid
+		if !(crafter.CheckKeys(*xiv) && crafter.CheckFlags(*xiv)) {
 			os.Exit(1)
 		}
 
--- pkg/crafter/validate.go
+++ pkg/crafter/validate.go
@@ -4,6 +4,72 @@ import (
 	"fmt"
 )
 
+var REQUIRED_FLAGS = [5]string{
+	"macro1",
+	"confirm",
+	"cancel",
+	"startPause",
+	"stop",
+}
+
+var ACCEPTED_KEYS = [55]string{
+	"",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"0",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"j",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+	"p",
+	"q",
+	"r",
+	"s",
+	"t",
+	"u",
+	"v",
+	"w",
+	"x",
+	"y",
+	"z",
+	"insert",
+	"delete",
+	"home",
+	"end",
+	"pageup",
+	"pagedown",
+	"f1",
+	"f2",
+	"f3",
+	"f4",
+	"f5",
+	"f6",
+	"f7",
+	"f8",
+	"f9",
+	"f10",
+	"f11",
+	"f12",
+}
+
 func CheckKeys(xiv XIVCrafter) bool {
 	SUCCESS := 0
 
@@ -20,73 +86,6 @@ func CheckKeys(xiv XIVCrafter) bool {
 }
 
 func checkKey(flag string, key string) int {
-	// required flags cannot use empty strings as keys
-	REQUIRED_FLAGS := [5]string{
-		"macro1",
-		"confirm",
-		"cancel",
-		"startPause",
-		"stop",
-	}
-
-	ACCEPTED_KEYS := [55]string{
-		"",
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-		"0",
-		"a",
-		"b",
-		"c",
-		"d",
-		"e",
-		"f",
-		"g",
-		"h",
-		"i",
-		"j",
-		"k",
-		"l",
-		"m",
-		"n",
-		"o",
-		"p",
-		"q",
-		"r",
-		"s",
-		"t",
-		"u",
-		"v",
-		"w",
-		"x",
-		"y",
-		"z",
-		"insert",
-		"delete",
-		"home",
-		"end",
-		"pageup",
-		"pagedown",
-		"f1",
-		"f2",
-		"f3",
-		"f4",
-		"f5",
-		"f6",
-		"f7",
-		"f8",
-		"f9",
-		"f10",
-		"f11",
-		"f12",
-	}
-
 	for _, KEY := range ACCEPTED_KEYS {
 		if KEY == key {
 			for _, FLAG := range REQUIRED_FLAGS {
@@ -95,6 +94,7 @@ func checkKey(flag string, key string) int {
 					if key == "" {
 						s := fmt.Sprintf("ERROR: FLAG %s SET WITH NO KEY", flag)
 						fmt.Println(s)
+						return 1
 					}
 
 					return 0
@@ -108,4 +108,45 @@ func checkKey(flag string, key string) int {
 	s := fmt.Sprintf("ERROR: FLAG %s SET WITH INVALID KEY %s", flag, key)
 	fmt.Println(s)
 	return 1
+}
+
+func CheckFlags(xiv XIVCrafter) bool {
+	SUCCESS := 0
+
+	// check Macro1
+	SUCCESS += checkFlag("macro1Duration", xiv.Macro1Duration)
+
+	// check Macro2
+	if xiv.Macro2 != "" {
+		SUCCESS += checkFlag("macro2Duration", xiv.Macro2Duration)
+	}
+
+	// check FoodDuration
+	if xiv.Food != "" {
+		SUCCESS += checkFlag("foodDuration", xiv.FoodDuration)
+	}
+
+	return SUCCESS == 0
+}
+
+func checkFlag(flag string, duration int) int {
+	if flag == "macro1Duration" || flag == "macro2Duration" {
+		if duration <= 0 {
+			s := fmt.Sprintf("ERROR: FLAG %s SET WITH INVALID DURATION %d", flag, duration)
+			fmt.Println(s)
+			return 1
+		}
+	} else if flag == "foodDuration" {
+		switch duration {
+		case 30:
+		case 40:
+		case 45:
+		default:
+			s := fmt.Sprintf("ERROR: FLAG %s SET WITH INVALID DURATION %d", flag, duration)
+			fmt.Println(s)
+			return 1
+		}
+	}
+
+	return 0
 }
